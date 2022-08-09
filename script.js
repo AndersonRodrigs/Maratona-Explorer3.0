f22cf93b69059402cf8a934f5b809806817a7bf0
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-08-09 20:29:17 -0300

    Alteracoes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function theme() {
-  let element
+  let element = document.body
+  element.classList.toggle('theme_B')
 }
+
+const checkbox = document.querySelector('.checkbox')
+
+checkbox.addEventListener('click', e => {
+  if (checkbox.classList.contains('on')) {
+    checkbox.setAttribute('aria-checked', 'false')
+  } else {
+    checkbox.setAttribute('aria-checked', 'true')
+  }
+
+  checkbox.classList.toggle('on')
+})
